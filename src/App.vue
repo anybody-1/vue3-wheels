4b42333dba9362ecfb64e1421f61566b2b1b9651
lvshuangyong <1721446288@qq.com>
2020-08-14 17:33:07 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Lv />
+</template>
+
+<script>
+import Lv from "./components/Lv.vue";
+
+export default {
+  name: "App",
+  components: {
+    Lv,
+  },
+};
+</script>
